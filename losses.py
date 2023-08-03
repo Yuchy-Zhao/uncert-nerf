remove HDR part and modify opacity gradient bug

losses.py
@@ -53,14 +53,16 @@ class NeRFLoss(nn.Module):
             d['depth'] = torch.abs(results['depth']-target['depth'])
 
         if kwargs.get('use_uncertainty', True):
+            # torch.mean((1 / (2*(uncert+1e-9).unsqueeze(-1))) *((x - y) ** 2)) + 0.5*torch.mean(torch.log(uncert+1e-9)) + w * alpha.mean() + 4.0
             mask = (results['uncert'] == 0).detach()
-            d['uncert'] = ((results['rgb'][~mask]-target['rgb'][~mask])**2) / \
-                        (2*(results['uncert'][~mask]+1e-9).unsqueeze(-1)) + \
-                        torch.log(results['uncert'][~mask]+1e-9).unsqueeze(-1) / 2
+            u = results['uncert'].unsqueeze(-1) + 1e-10
+            d['uncert'] = d['rgb'][~mask] / u[~mask] \
+                           + torch.log(u[~mask]) / 2
 
-        o = results['opacity']+1e-10
-        # # encourage opacity to be either 0 or 1 to avoid floater
-        d['opacity'] = self.lambda_opacity*(-o*torch.log(o))
+        if self.lambda_distortion > 0:
+            o = results['opacity']+1e-10
+            # # encourage opacity to be either 0 or 1 to avoid floater
+            d['opacity'] = self.lambda_opacity*(-o*torch.log(o))
 
         if self.lambda_distortion > 0:
             d['distortion'] = self.lambda_distortion * \

models/networks.py
@@ -10,10 +10,8 @@ from .rendering import NEAR_DISTANCE
 
 
 class NGP(nn.Module):
-    def __init__(self, scale, rgb_act='Sigmoid'):
+    def __init__(self, scale):
         super().__init__()
-
-        self.rgb_act = rgb_act
 
         # scene bounding box
         self.scale = scale
@@ -94,26 +92,11 @@ class NGP(nn.Module):
                 network_config={
                     "otype": "FullyFusedMLP",
                     "activation": "ReLU",
-                    "output_activation": self.rgb_act,
+                    "output_activation": "Sigmoid",
                     "n_neurons": 64,
                     "n_hidden_layers": 2,
                 }
             )
-
-        if self.rgb_act == 'None': # rgb_net output is log-radiance
-            for i in range(3): # independent tonemappers for r,g,b
-                tonemapper_net = \
-                    tcnn.Network(
-                        n_input_dims=1, n_output_dims=1,
-                        network_config={
-                            "otype": "FullyFusedMLP",
-                            "activation": "ReLU",
-                            "output_activation": "Sigmoid",
-                            "n_neurons": 64,
-                            "n_hidden_layers": 1,
-                        }
-                    )
-                setattr(self, f'tonemapper_net_{i}', tonemapper_net)
 
     def density(self, x, return_feat=False):
         """
@@ -130,29 +113,6 @@ class NGP(nn.Module):
         if return_feat: return sigmas, h
         return sigmas
 
-    def log_radiance_to_rgb(self, log_radiances, **kwargs):
-        """
-        Convert log-radiance to rgb as the setting in HDR-NeRF.
-        Called only when self.rgb_act == 'None' (with exposure)
-
-        Inputs:
-            log_radiances: (N, 3)
-
-        Outputs:
-            rgbs: (N, 3)
-        """
-        if 'exposure' in kwargs:
-            log_exposure = torch.log(kwargs['exposure'])
-        else: # unit exposure by default
-            log_exposure = 0
-
-        out = []
-        for i in range(3):
-            inp = log_radiances[:, i:i+1]+log_exposure
-            out += [getattr(self, f'tonemapper_net_{i}')(inp)]
-        rgbs = torch.cat(out, 1)
-        return rgbs
-
     def forward(self, x, d, **kwargs):
         """
         Inputs:
@@ -168,13 +128,6 @@ class NGP(nn.Module):
         d = self.dir_encoder((d+1)/2)
         rgbs = self.rgb_net(torch.cat([d, h], 1))
         uncerts = TruncExp.apply(self.uncert_net(h)[:,0])
-
-        if self.rgb_act == 'None': # rgbs is log-radiance
-            if kwargs.get('output_radiance', False): # output HDR map
-                rgbs = TruncExp.apply(rgbs)
-            else: # convert to LDR using tonemapper networks
-                rgbs = self.log_radiance_to_rgb(rgbs, **kwargs)
-
         return sigmas, rgbs, uncerts
 
     @torch.no_grad()

train.py
@@ -31,6 +31,7 @@ from torchmetrics import (
 from torchmetrics.image.lpip import LearnedPerceptualImagePatchSimilarity
 
 # pytorch-lightning
+from pytorch_lightning.utilities import seed
 from pytorch_lightning.plugins import DDPPlugin
 from pytorch_lightning import LightningModule, Trainer
 from pytorch_lightning.callbacks import TQDMProgressBar, ModelCheckpoint
@@ -54,6 +55,7 @@ class NeRFSystem(LightningModule):
     def __init__(self, hparams):
         super().__init__()
         self.save_hyperparameters(hparams)
+        seed.seed_everything(0)
 
         self.warmup_steps = 256
         self.update_interval = 16
@@ -68,8 +70,7 @@ class NeRFSystem(LightningModule):
             for p in self.val_lpips.net.parameters():
                 p.requires_grad = False
 
-        rgb_act = 'None' if self.hparams.use_exposure else 'Sigmoid'
-        self.model = NGP(scale=self.hparams.scale, rgb_act=rgb_act)
+        self.model = NGP(scale=self.hparams.scale)
         G = self.model.grid_size
         self.model.register_buffer('density_grid',
             torch.zeros(self.model.cascades, G**3))
@@ -95,8 +96,6 @@ class NeRFSystem(LightningModule):
                   'random_bg': self.hparams.random_bg}
         if self.hparams.scale > 0.5:
             kwargs['exp_step_factor'] = 1/256
-        if self.hparams.use_exposure:
-            kwargs['exposure'] = batch['exposure']
 
         return render(self.model, rays_o, rays_d, **kwargs)
 
@@ -168,12 +167,6 @@ class NeRFSystem(LightningModule):
         kwargs = {'use_depth': self.hparams.use_depth,
                   'use_uncertainty': self.hparams.use_uncertainty}
         loss_d = self.loss(results, batch, **kwargs)
-        if self.hparams.use_exposure:
-            zero_radiance = torch.zeros(1, 3, device=self.device)
-            unit_exposure_rgb = self.model.log_radiance_to_rgb(zero_radiance,
-                                    **{'exposure': torch.ones(1, 1, device=self.device)})
-            loss_d['unit_exposure'] = \
-                0.5*(unit_exposure_rgb-self.train_dataset.unit_exposure_rgb)**2
         loss = sum(lo.mean() for lo in loss_d.values())
 
         with torch.no_grad():